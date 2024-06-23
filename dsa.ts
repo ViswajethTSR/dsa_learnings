9f0e903ad22d1c6b55328458517cdf7a3a718e5e
ViswajethTSR <viswa.dev.in@gmail.com>
2024-06-23 14:30:02 +0530

    BIrthday calculator is added

--- dsa.ts
+++ dsa.ts
@@ -110,7 +110,6 @@ class DataStructureLearnings {
         arr.sort((a, b) => a - b);
         let temp: number = 0;
 
-        console.log(arr);
         for (let i = 0; i < arr.length - 1; i += 2) {
             temp = arr[i];
             arr[i] = arr[i + 1];
@@ -129,4 +128,29 @@ class DataStructureLearnings {
     }
 }
 
-new DataStructureLearnings("Viswajeth TSR", 21).executionMethod();
+function calculateAge(birthdate: Date): number {
+    const today = new Date();
+    let age = today.getFullYear() - birthdate.getFullYear();
+    const monthDiff = today.getMonth() - birthdate.getMonth();
+    const dayDiff = today.getDate() - birthdate.getDate();
+
+    if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
+        age--;
+    }
+
+    return age;
+}
+
+const developer: IDeveloper = {
+    name: "Viswajeth TSR",
+    email: "viswa.dev.in@gmail.com",
+    age: calculateAge(new Date(2003, 5, 4))
+}
+
+new DataStructureLearnings(developer.name, developer.age).executionMethod();
+
+interface IDeveloper {
+    name: string,
+    email: string,
+    age: number,
+}
